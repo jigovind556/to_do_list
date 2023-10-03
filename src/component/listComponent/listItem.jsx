c3f4a69fa186164f0edbe5ae2618f1178775292e
jigovind556 <12113078@nitkkr.ac.in>
2023-10-03 12:22:24 +0530

    added mark as done and edit feature

--- src/component/listComponent/listItem.jsx
+++ src/component/listComponent/listItem.jsx
@@ -1,29 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../../css/list-item.css";
 
 const ListItem = ({ task, onEdit, onToggleComplete, onDelete }) => {
+  const [toggle,setToggle]=useState(false);
   const handleEditClick = () => {
-    onEdit(task.id, "hello");
+    var txt=document.getElementById('editText').value;
+    if (txt.trim() !== '') {
+      onEdit(txt);
+    }
+    
+    setToggle(false);
+    document.getElementById('editText').value="";
   };
 
   const handleDeleteClick = () => {
     onDelete(task.id);
+
+    
   };
+  const changeToggle=()=>{
+    setToggle(!toggle);
+  }
 
   return (
     <div className="list-item">
       <input
         type="checkbox"
+        className='list-item-checkbox'
+        style={{position:"static"}}
+        
         checked={task.completed}
         onChange={() => onToggleComplete(task.id)}
       />
-      <span className={`task-text ${task.completed ? 'completed' : ''}`}>
+      {toggle?
+      <input type='text' id='editText'/>
+      :<span className={`task-text ${task.completed ? 'completed' : ''}`}>
         {task.text}
-      </span>
+      </span>}
       <div className="actions">
-        <button className="edit-button" onClick={handleEditClick}>
-          Edit
+        {toggle?<button className="edit-button" onClick={handleEditClick}>
+          Set
         </button>
+        :<button className="edit-button" onClick={changeToggle}>
+          Edit
+        </button>}
         <button className="delete-button" onClick={handleDeleteClick}>
           Delete
         </button>
